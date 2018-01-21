eed759623ec97817b5f61757025dff14e04e9944
Mike G <mgrutsch@gmail.com>
2018-01-21 13:08:13 -0600

    A

--- binance_config.py
+++ binance_config.py
@@ -5,8 +5,8 @@ Created on Tue Oct 17 19:20:42 2017
 @author: Mikey
 """
 
-api_key = 'xYc94zKmlKmy5dGs3lSzwaATcv3PROO6RqbsZrHGvU1uXu6D3NKMLV3RBeKzQBQS'
-api_secret = 'eLBRTD8OArqHjZgefc5vD0eXhIXK13qqQor8DwkwP2moJmy4I5yew02NFNFduyC0'
+api_key = 'API_KEY'
+api_secret = 'API_SECRET'
 recv_window = 100000  #Open window of 10 seconds
 no_trade = ['LRC', 'BNB', 'ZEC', 'ETC', 'BTG', 'QTUM', 'NEO', 'BCC', 'HSR','ETC', 'GAS', 'LINK'] #EXCLUDE 
 btc_trade_limit = .002 #Minimum notional value for BTC transaction is 0.001
